4734241e14d19aeaace52ca7dbc6000a36d84b59
Tharusha Iduwara <tharushaiduwara1234@gmail.com>
2026-06-10 12:16:45 +0530

    change

--- config/database.php
+++ config/database.php
@@ -1,12 +1,14 @@
+```php
 <?php
-$conn = new mysqli(
-    getenv('DB_HOST'),
-    getenv('DB_USER'),
-    getenv('DB_PASS'),
-    getenv('DB_NAME'),
-    (int)getenv('DB_PORT')
-);
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'movieflix';
 
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
